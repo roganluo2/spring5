6a1197034e374a90780438c74f4ba9a0c3976574
rogan.luo <rogan.luo@corp.to8to.com>
2019-03-26 18:28:38 +0800

    init module

---
+++ src/main/java/com/gperedu/spring5/App.java
@@ -0,0 +1,13 @@
+package com.gperedu.spring5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
